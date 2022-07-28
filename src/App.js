adcaca8a5355799bffe94b31bf4e89ee71c45cb4
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-07-28 22:55:58 +0100

    clean up and map method added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [monsters, setMonsters] = useState({
+    monsters: [
+    {
+      name: 'Linda'
+    },
+    {
+      name: 'Frank'
+    },
+    {
+      name: 'Jacky'
+    }
+  ]})
+
   return (
     <>
-
+      {
+        monsters.monsters.map( (monster, index) => {
+          return <h1 key={index}>{monster.name}</h1>
+          }
+        )
+      }
     </>
   );
 }
